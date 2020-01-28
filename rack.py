a02c86ee9675401e660be9b01075419ed29d37a0
Paul Maynard <Paul.Maynard001@umb.edu>
2020-01-28 10:40:21 -0500

    basic evaluation

--- rack.py
+++ rack.py
@@ -1,12 +1,13 @@
-from typing import ChainMap, Sequence, List, Iterator
+from typing import ChainMap, Sequence, List, Iterator, Iterable, Callable, Dict
 from abc import ABC, abstractmethod
 import re
+import sys
 
 Context = ChainMap[str, 'Value']
 
 
 class Value(ABC):
-    def eval(self, ctx: Context) -> 'Value':
+    def run(self, ctx: Context) -> 'Value':
         return self
 
 
@@ -21,9 +22,17 @@ class VList(Value):
 
     def __len__(self) -> int:
         return len(self.elems)
-    
+
     def __str__(self):
         return f"({' '.join(map(str, self.elems))})"
+
+    def run(self, ctx: Context) -> Value:
+        head = self.elems[0].run(ctx)
+        args = [e.run(ctx) for e in self.elems[1:]]
+        if isinstance(head, Fun):
+            return head.call(args)
+        else:
+            raise ValueError
 
 
 class Num(Value):
@@ -44,6 +53,12 @@ class Num(Value):
     def __div__(self, other: 'Num') -> 'Num':
         return Num(self.val / other.val)
 
+    def __str__(self):
+        if self.val.imag == 0:
+            return str(self.val.real)
+        else:
+            return str(self.val)
+
 
 class Name(Value):
     name: str
@@ -51,8 +66,11 @@ class Name(Value):
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def eval(self, ctx: Context) -> Value:
+    def run(self, ctx: Context) -> Value:
         return ctx[self.name]
+
+    def __str__(self):
+        return self.name
 
 
 class Fun(Value):
@@ -64,17 +82,21 @@ class Fun(Value):
 
 
 class PyFun(Fun):
-    def __init__(self, fun):
+    def __init__(self, name: str, fun: Callable[..., Value]):
+        self.name = name
         self.fun = fun
 
     def call(self, args) -> Value:
         return self.fun(*args)
 
+    def __str__(self):
+        return f"#<function:{self.name}>"
+
 
 def parse(inp: str) -> Iterator[Value]:
     toks = iter(re.findall(r'\(|\)|[^()\s]+', inp))
     for t in toks:
-        yield _parse(next(toks), toks)
+        yield _parse(t, toks)
 
 
 def _parse(head: str, toks: Iterator[str]) -> Value:
@@ -88,10 +110,29 @@ def _parse(head: str, toks: Iterator[str]) -> Value:
         else:
             raise ValueError
     elif head[0] in '0123456789':
-        if re.fullmatch(r'[+-]?(?:0[xbo])\d+', head):
+        if re.fullmatch(r'[+-]?(?:0[xbo])?\d+', head):
             return Num(int(head))
         else:
             return Num(complex(head))
     else:
         return Name(head)
 
+
+stdlib: Dict[str, Value] = {
+    '+': PyFun('+', lambda *args: Num(sum(a.val for a in args)))
+}
+
+
+def run(exps: Iterable[Value],
+        ctx: Context = None, show: bool = False) -> Value:
+    res = VList([])
+    if ctx is None:
+        ctx = ChainMap({}, stdlib)
+    for e in exps:
+        res = e.run(ctx)
+        if show:
+            print(res)
+    return res
+
+
+run(parse(sys.argv[1]), None, True)
